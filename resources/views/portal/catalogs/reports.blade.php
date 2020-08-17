3b9775a2753b3d97ba83dc6026932aaec31d5a78
Guillermo Croppi <guillermocroppi@gmail.com>
2020-08-17 19:57:52 -0300

    Typo fix

--- resources/views/portal/catalogs/reports.blade.php
+++ resources/views/portal/catalogs/reports.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 <div class="container">
   <div class="py-5">
-  <h2 class="is-600 mb-3">Catalogo de reportes</h2>
+  <h2 class="is-600 mb-3">Catálogo de reportes</h2>
   <search-reports fetch-url="{{route('apiService.reports')}}" querystring="">
     @include('partials.loading')
   </search-reports>
