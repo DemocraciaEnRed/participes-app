a306b6b19ec56400acca607d52060910d2eaa26f
Guillermo Croppi <guillermocroppi@gmail.com>
2020-08-26 15:46:39 -0300

    Organization Carrousel update

--- resources/js/components/portal/objective/OrganizationCarrousel.vue
+++ resources/js/components/portal/objective/OrganizationCarrousel.vue
@@ -3,9 +3,9 @@
     <swiper-slide v-for="org in slides" :key="`org-${org.id}`">
       <div class="media p-1 d-flex align-items-stretch">
         <img
-          :src="org.logo ? `/${org.logo.path}` : '/img/default-avatar.png'"
+          :src="org.logo ? `${org.logo.path}` : '/img/default-avatar.png'"
           class="mr-3 rounded"
-          style="width: 64px; height: 64px;"
+          style="width: 64px; height: auto;"
         />
         <div class="media-body mr-5">
           <h6 class="is-600">{{org.name}}</h6>
